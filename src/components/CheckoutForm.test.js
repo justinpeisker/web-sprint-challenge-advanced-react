4737c75a810b57a3f0035c61761c336c278b94bf
Justin Peisker <justinpeisker@gmail.com>
2021-12-03 08:57:15 -0800

    tests finished

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -5,6 +5,35 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
 
-test("shows success message on submit with form details", () => {});
+    render(<CheckoutForm />);
+});
+
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+
+    const firstName = screen.queryByLabelText("First Name:");
+    userEvent.type(firstName, "Justin");
+
+    const lastName = screen.queryByLabelText("Last Name:");
+    userEvent.type(lastName, "Peisker");
+
+    const address = screen.queryByLabelText("Address:");
+    userEvent.type(address, "123 Main St.");
+
+    const city = screen.queryByLabelText("City:");
+    userEvent.type(city, "Somewhereville");
+
+    const state = screen.queryByLabelText("State:");
+    userEvent.type(state, "CA");
+
+    const zip = screen.queryByLabelText("zip:");
+    userEvent.type(state, "00000");
+
+    const button = screen.queryByRole("button");
+    userEvent.click(button);
+
+    const message = screen.getByText(/You have ordered some plants! Woo-hoo/)
+    expect(message).toBeInTheDocument();
+});
